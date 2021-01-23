447b85b65467b32475632c09dce7b24715ee4eff
Haixiang <haixiang6123@gmail.com>
2021-01-23 08:30:10 +0800

    feat: 添加 ErrorBoundary 的例子

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import {ErrorBoundary, FallbackProps} from 'react-error-boundary'
 
-function App() {
+const ErrorFallback = ({error, resetErrorBoundary}: FallbackProps) => {
+  return (
+    <div role="alert">
+      <p>出错啦</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
+}
+
+const Bomb = () => {
+  throw new Error('new error');
+}
+
+const App = () => {
   return (
     <div className="App">
-      Hello World
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => {
+          // reset the state of your app so the error doesn't happen again
+        }}
+      >
+        <Bomb />
+      </ErrorBoundary>
     </div>
   );
 }
